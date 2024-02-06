a546df45d54c445ba6141c315f6d17a79cebe346
Gor Shipaktsyan <gor.shipaktsyan@groundtruth.com>
2024-02-06 10:02:30 +0400

    fix

--- src/view/pages/books/Books.js
+++ src/view/pages/books/Books.js
@@ -29,7 +29,7 @@ export default function Books() {
       payload: { currentChapter },
     });
     setIsOpen(false);
-    navigate("/chapter");
+    navigate("/armbible/chapter");
   }
   return (
     <StyledContainer>
--- src/view/pages/chapter/Chapter.js
+++ src/view/pages/chapter/Chapter.js
@@ -82,7 +82,7 @@ export default function Chapter() {
   );
 
   function handleBack() {
-    navigate("/");
+    navigate("/armbible");
   }
   return (
     <StyledContainer {...handlers}>
